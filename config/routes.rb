dd90e0d37ff62ec010a571f6faf12e3b74f68baa
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-16 20:55:49 +0300

    Add connections to pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :tickets
   resources :users
   root 'static_pages#home'
+  get  '/home',  to: 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  get  '/profile',  to: 'users#show'
+  get  '/lines',  to: 'lines#index'
 end
