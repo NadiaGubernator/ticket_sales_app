a934891aaec6b5100eb46d99b6d45466592c6f9b
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-17 00:25:27 +0300

    Upgrade user profile

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,13 +10,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-#  test "should create user" do
-#    assert_difference('User.count') do
-#      post users_url, params: { user: { admin: @user.admin, email: @user.email, name: @user.name, password_digest: @user.password_digest, surname: @user.surname } }
-#    end
-#    assert_redirected_to user_url(User.last)
-#  end
-
   test "should show user" do
     get user_url(@user)
     assert_response :success
