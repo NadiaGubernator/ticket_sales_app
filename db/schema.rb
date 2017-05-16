64fbec4bc5a1bb3831488af7a07c075042e1b110
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-16 21:54:39 +0300

    Develop User model, create sign up requirements and secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516141942) do
+ActiveRecord::Schema.define(version: 20170516184152) do
 
   create_table "lines", force: :cascade do |t|
     t.integer  "seats_total"
