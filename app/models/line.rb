1b4fc3afaf0ff4e3d56641895869d0bf14ab7fce
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-17 21:58:40 +0300

    Start work with lines and tickets

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,3 +1,9 @@
 class Line < ApplicationRecord
   has_many :tickets
+  default_scope -> { order(created_at: :asc) }
+  validates :seats_total, presence: true
+  validates :departure_city, presence: true, length: { maximum: 50 }
+  validates :departure_time, presence: true
+  validates :arrival_city, presence: true, length: { maximum: 50 }
+  validates :arrival_time, presence: true
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :user
-  belongs_to :line
+  belongs_to :line, dependent: :destroy
 end
