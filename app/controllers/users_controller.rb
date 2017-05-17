c0a2ddb9a36585a0da96bd8c781b6d0aadf33b01
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-17 15:00:08 +0300

    Use SSL and the Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name, :surname, :email, :password,
                                    :password_confirmation)
     end
 end
