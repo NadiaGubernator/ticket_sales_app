a02d4b4b657c64d9dd3ca333d0dba2d6d4b410e8
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-17 15:19:39 +0300

    Add beginning of login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :lines
   resources :tickets
   resources :users
-  root 'static_pages#home'
-  get  '/home',  to: 'static_pages#home'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-  get  '/profile',  to: 'users#show'
-  get  '/lines',  to: 'lines#index'  
+
+  root   'static_pages#home'
+  
+  get    '/home',  to: 'static_pages#home'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/profile',  to: 'users#show'
+  get    '/lines',  to: 'lines#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
