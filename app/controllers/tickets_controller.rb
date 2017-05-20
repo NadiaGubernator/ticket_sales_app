21d40492e602712c212cbf7f6fd3ad6d305d1288
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-20 21:42:33 +0300

    Add starting point for purchasing tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,21 @@
 class TicketsController < ApplicationController
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+  helper_method :create
 
   def show
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+        print 'Ticket was successfully bought.'
+    else
+        render :new
+    end
   end
 
   private
