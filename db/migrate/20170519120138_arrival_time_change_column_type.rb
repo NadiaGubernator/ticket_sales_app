96ab7b49a98b23e5db0fe280da84e03f35e94e66
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-05-20 17:10:24 +0300

    Delete migrate file

--- db/migrate/20170519120138_arrival_time_change_column_type.rb
+++
@@ -1,9 +0,0 @@
-class ArrivalTimeChangeColumnType < ActiveRecord::Migration[5.0]
-  def up
-    change_column :lines, :arrival_time, :datetime
-  end
-
-  def down
-    change_column :lines, :arrival_time, :time
-  end
-end
